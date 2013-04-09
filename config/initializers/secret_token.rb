0fd0fb7e2512ea68e739e362b8c72814022628f7
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-09 15:59:28 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsvp::Application.config.secret_token = '94e15ddc8f3e1bc2c8209c66ef8794c160e88d82e68a032eed277b74ca7f9a13093446667885147f9ca9014e6f4faa9046d99b3cdfef83f06c3faeea0c809b38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rsvp::Application.config.session_store :cookie_store, key: '_rsvp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rsvp::Application.config.session_store :active_record_store
