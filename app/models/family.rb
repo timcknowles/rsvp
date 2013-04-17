6260470f09d3e505b5ff8e62698568cc916e08a3
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-17 20:49:39 +0100

    dependent destroy

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,7 +2,7 @@ class Family < ActiveRecord::Base
 
   has_secure_password
   attr_accessible :name, :login_code, :password, :password_confirmation, :guests_attributes
-  has_many :guests
+  has_many :guests, :dependent => :destroy
   
   accepts_nested_attributes_for :guests, allow_destroy: false  
 
