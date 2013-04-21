1f6f72e2f3d23379acb877dd6e002a8992506ded
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-21 23:50:47 +0100

    syntax error on s3 fixed

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 AWS::S3::Base.establish_connection!(
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], 
-  :bucket => "skinnersrsvp"}
+  :bucket => ['skinnersrsvp']
 )
